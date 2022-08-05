acdb2f98d3a49b3e4e061d4faf1ce050dbee9d14
Chetan Chauhan-Sims <chetan.chauhansims@digital.cabinet-office.gov.uk>
2022-08-05 16:52:46 +0100

    first commit

---
+++ src/utils/constants.py
@@ -0,0 +1,9 @@
+import os
+
+# RAW_DATA_FOLDER_PATH = os.path.realpath(
+#     os.path.join(os.path.dirname(__file__), "..", "..", "data", "raw")
+# )
+
+RAW_DATA_FOLDER_PATH = os.path.realpath(
+    os.path.join(os.getcwd(), "data", "raw")
+)
